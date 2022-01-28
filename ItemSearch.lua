975573de160891a29e2cbcdd7dee91d2db538d71
Anonomit <anonomit@gmail.com>
2022-01-27 21:53:15 -0500

    Added more items to cache

--- ItemSearch.lua
+++ ItemSearch.lua
@@ -14,7 +14,25 @@ LootReserve.ItemSearch =
     Speed           = 10,
     
     IDLists = {
-        {min = 1, max = 40000},
+        {min =      1, max =  34900},
+        {min =  35300, max =  35600},
+        {min =  37800, max =  38300},
+        {min =  38450, max =  38650},
+        {min =  39149, max =  39149},
+        {min =  39476, max =  39477},
+        {min =  39656, max =  39656},
+        {min =  43516, max =  43516},
+        {min = 122270, max = 122290},
+        {min = 172070, max = 172070},
+        {min = 180089, max = 180089},
+        {min = 184865, max = 184938},
+        {min = 185686, max = 185693},
+        {min = 185848, max = 186163},
+        {min = 186682, max = 186683},
+        {min = 187048, max = 187130},
+        {min = 187435, max = 187435},
+        {min = 187714, max = 187815},
+        {min = 190179, max = 190325},
     },
 };
 
@@ -149,7 +167,7 @@ function LootReserve.ItemSearch:Search(query)
 end
 
 function LootReserve.ItemSearch:GetProgress()
-    return 100 * self.LoadedItems / math.max(1, self.MaxItems), self.LoadState;
+    return math.min(self.LoadedItems / math.max(1, self.MaxItems), 1) * 100, self.LoadState;
 end
 
 function LootReserve.ItemSearch:SetSpeed(speed)
